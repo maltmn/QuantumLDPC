4fbf4e95668f8bfea7e5aff2e57db01b7d774491
maltmn <maltmn@yahoo.com>
2025-11-21 20:19:36 -0800

    Create fnn_model.py

---
+++ fnn/fnn_model.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+class FNNDecoder(nn.Module):
+  def __init__(self, syndrome_dim, n_qubits):
+    super().__init__()
+    self.net = nn.Sequential(
+      nn.Linear(syndrome_dim, 256), # input
+      nn.ReLU(),  # activation
+      nn.Linear(256, n_qubits) # output
+    )
+
+  def forward(self, s):
+    return self.net(s)
