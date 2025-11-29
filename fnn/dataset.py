a7836fb08b21de8354dd757be60dae33ffb0ec90
Matteo <maltmn@gmail.com>
2025-11-28 23:26:01 -0800

    Comments

--- fnn/dataset.py
+++ fnn/dataset.py
@@ -6,7 +6,7 @@ from panqec.codes import surface_2d
 def generate_errors(num_samples, n_qubits, pX, pY, pZ):
 # PauliErrorModel(pX, pY, pZ)
   errors = np.random.choice(
-    [0,1,2,3], # I,X,Y,Z
+    [0,1,2,3], # I,X,Y,Z : none, X, both, Z
     size = (num_samples, n_qubits),
     p = [1-pX-pY-pZ, pX, pY, pZ]
   )
@@ -30,7 +30,8 @@ def syndrome(H, e):
   #6 entries for d=3
   #one syndrome -> many different e's
 
-code = surface_2d.RotatedPlanar2DCode(3)
+#d = L-1 = 3
+code = surface_2d.RotatedPlanar2DCode(4)
 
 #H matrices determine which qubits are checked for errors
 #1: qubit included in syndrome calculation
@@ -42,7 +43,11 @@ Hz = code.Hz.toarray()
 print(code.Hx.shape)
 print(code.Hz.shape)
 
-#for d=3, H = (4 rows, 9 columns) = (checks, qubits)
+print("Dataset loaded from", __file__)
+print("Lattice used", code.d)
+
+#d=2, H = (4 rows, 9 columns) = (checks, qubits)
+#d=3, H = (7 or 8 rows, 16 columns) 
 
 '''
  Hx = np.array([
--- fnn/fnn_model.py
+++ fnn/fnn_model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+#syndrome_dim = number of stabilizer rows
 class FNNDecoder(nn.Module):
   def __init__(self, syndrome_dim, n_qubits):
     super().__init__()
@@ -8,15 +9,32 @@ class FNNDecoder(nn.Module):
     self.n_qubits = n_qubits
     
     self.net = nn.Sequential(
-      nn.Linear(syndrome_dim, 1024), # input
-      nn.ReLU(),  # activation
-      nn.Linear(1024, 1024),
-      nn.ReLU(),
-      nn.Linear(1024, 2*n_qubits) # output both X and Z
+
+      # [5000, 15] -> [5000, 1024]
+      # convert s=[0,1,0,1] to h=[3, 7.2, 0.5, -2.3...] with 1024 entries 
+      nn.Linear(syndrome_dim, 1024), # input creates simple pattern
+
+      # [5000, 1024] -> [5000, 1024]
+      # keep +'s but d -'s become 0's
+      nn.ReLU(),  # first hidden activation, keeps meaningful results
+
+      # [5000, 1024] -> [5000, 1024]
+      #remix h=[3, 7.2, 0.5, 0...] to h=[4.9, 5, -3, 0.1...]
+      nn.Linear(1024, 1024), # hidden linear layer, creates more complex pattern
+
+      # [5000, 1024] -> [5000, 1024]
+      # keep +'s but d -'s become 0's
+      nn.ReLU(), # second activation, keeps meaningful results
+
+      # [5000, 1024] -> [5000, 32]
+      nn.Linear(1024, 2*n_qubits) # output 16 X and 16 Z predictions (32 entries)
     )
 
   def forward(self, s):
     logits = self.net(s)
-    eX_logits = logits[:, :self.n_qubits]
-    eZ_logits = logits[:, self.n_qubits:]
+    eX_logits = logits[:, :self.n_qubits] # first 16 columns
+    eZ_logits = logits[:, self.n_qubits:] # last 16 columns
     return eX_logits, eZ_logits
+
+#runs syndrome matrix (5000 samples, 15 columns) through the neural network
+#separates the 32 ouputs into eX:16 and eZ:16
